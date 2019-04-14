8d30b08201af2ff109d9af90575645cec6db9481
makubass <makubass@gmail.com>
2019-04-14 18:52:06 +0900

    Initial Commit.

---
+++ 01_InstallModules.ps1
@@ -0,0 +1,9 @@
+## Sharepointのモジュールバージョン確認
+Get-Module -Name Microsoft.Online.SharePoint.PowerShell -ListAvailable | Select Name,Version
+Install-Module -Name Microsoft.Online.SharePoint.PowerShell
+
+## Skypeモジュールダウンロードパス
+https://www.microsoft.com/en-us/download/details.aspx?id=39366
+
+## Teansモジュールのインストール
+Install-Module MicrosoftTeams
