192e15ded89dc197f9074cff725e638616702238
makubass <makubass@gmail.com>
2019-05-09 23:44:40 +0900

    "Get-Command -Module" added.

--- 02_AccessToO365.ps1
+++ 02_AccessToO365.ps1
@@ -5,15 +5,19 @@ $credential = Get-Credential
 Connect-MsolService -Credential $credential
 
 
-## Exchange Online への接続
+## Exchange Online への接続　※セッションは最後に必ず切断すること
 $exchangeSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri "https://outlook.office365.com/powershell-liveid/" -Credential $credential -Authentication "Basic" -AllowRedirection
 Import-PSSession $exchangeSession -DisableNameChecking
+## インポートしたコマンドの確認
+Get-Command -Module tmp_jp4o4bs0.bme
 
-## Sharepoint Online への接続
+## Sharepoint Online への接続　※セッションは最後に必ず切断すること
 Import-Module -Name Microsoft.Online.SharePoint.PowerShell -DisableNameChecking
 Connect-SPOService -Url https://$orgName-admin.sharepoint.com -credential $credential
+## インポートしたコマンドの確認
+Get-Command -Module Microsoft.Online.SharePoint.PowerShell
 
-## Skype Online への接続
+## Skype Online への接続　※セッションは最後に必ず切断すること
 Import-Module SkypeOnlineConnector
 $sfboSession = New-CsOnlineSession -Credential $credential
 Import-PSSession $sfboSession
@@ -22,7 +26,7 @@ Import-PSSession $sfboSession
 $SccSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.compliance.protection.outlook.com/powershell-liveid/ -Credential $credential -Authentication Basic -AllowRedirection
 Import-PSSession $SccSession -Prefix cc
 
-## Teams への接続
+## Teams への接続　※セッションは最後に必ず切断すること
 Connect-MicrosoftTeams
 
 ## Azure AD v2 PowerShell への接続
